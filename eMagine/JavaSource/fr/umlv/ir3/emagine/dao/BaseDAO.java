c77a5ade3ddd8a73bfe4735e2a426ab1f1295266
netangel <netangel@06c2f47b-b305-0410-bc58-bf68df72c600>
2005-11-18 15:42:26 +0000

    - CollectionScroller oublié lors du précédent commit - BaseDAO enlevage d'un warning d'import

--- eMagine/JavaSource/fr/umlv/ir3/emagine/dao/BaseDAO.java
+++ eMagine/JavaSource/fr/umlv/ir3/emagine/dao/BaseDAO.java
@@ -3,13 +3,7 @@
  */
 package fr.umlv.ir3.emagine.dao;
 
-import java.util.Collection;
 
-
-/**
- * @author Administrateur
- *
- */
 //CRUD Design pattern
 public interface BaseDAO<ObjectType> {
     public void create(ObjectType object);
