facb6cb7b1ea03935f08b332d9c7cfaa836ad0d6
lbarbisan <lbarbisan@06c2f47b-b305-0410-bc58-bf68df72c600>
2005-11-19 11:11:01 +0000

    Configuratino Hibernate

--- eMagine/JavaSource/fr/umlv/ir3/emagine/user/Profile.java
+++ eMagine/JavaSource/fr/umlv/ir3/emagine/user/Profile.java
@@ -1,12 +1,19 @@
 package fr.umlv.ir3.emagine.user;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Profile {
 	private long id;
+	private String Name;
 	
 	@Override
 	public boolean equals(Object obj) {
 		return id == ((Profile)obj).getId();
 	}
+	
+	@Id
 	public long getId() {
 		return id;
 	}
--- eMagine/JavaSource/fr/umlv/ir3/emagine/user/User.java
+++ eMagine/JavaSource/fr/umlv/ir3/emagine/user/User.java
@@ -1,5 +1,8 @@
 package fr.umlv.ir3.emagine.user;
 
+import javax.persistence.Entity;
+
+@Entity
 public class User {
 
 }
