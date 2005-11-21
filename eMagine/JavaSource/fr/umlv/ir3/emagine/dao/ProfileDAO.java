71a11dc194a67cc657cc1b8301715cebbf396552
lbarbisan <lbarbisan@06c2f47b-b305-0410-bc58-bf68df72c600>
2005-11-21 17:53:36 +0000

    Mise à jour des données de création pour la base de données

--- eMagine/JavaSource/fr/umlv/ir3/emagine/dao/ProfileDAO.java
+++
@@ -1,9 +0,0 @@
-package fr.umlv.ir3.emagine.dao;
-
-import java.util.Collection;
-
-import fr.umlv.ir3.emagine.user.Profile;
-
-public interface ProfileDAO extends BaseDAO<Profile> {
-	public Collection<Profile> getProfiles();
-}
--- eMagine/JavaSource/fr/umlv/ir3/emagine/util/AjaxScrollAction.java
+++
@@ -1,9 +0,0 @@
-package fr.umlv.ir3.emagine.util;
-
-import org.apache.struts.action.Action;
-
-public class AjaxScrollAction extends Action {
-	// TODO : implémenter l'action getCollection(String ScrollableID, int index)
-	// TODO : implémenter l'action getNbResultsByPage(String ScrollableID, int nbResultsByPage)
-	// TODO : TLD : scrollable / class / name / scope
-}
