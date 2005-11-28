7d9d6d411a43b4daa821de1ad9f153bdfa6919bc
lbarbisan <lbarbisan@06c2f47b-b305-0410-bc58-bf68df72c600>
2005-11-28 06:41:15 +0000

    Concept du gestion de modification terminé, à tester, utilise l'interceptor de Hibernate

--- eMagine/JavaSource/fr/umlv/ir3/emagine/util/HibernateUtils.java
+++ eMagine/JavaSource/fr/umlv/ir3/emagine/util/HibernateUtils.java
@@ -7,6 +7,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hibernate.EntityMode;
 import org.hibernate.HibernateException;
+import org.hibernate.Interceptor;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -14,6 +15,7 @@ import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.metadata.ClassMetadata;
 
 import fr.umlv.ir3.emagine.modification.FieldModification;
+import fr.umlv.ir3.emagine.modification.ModificationInterceptor;
 import fr.umlv.ir3.emagine.util.base.BaseEntity;
 
 
@@ -26,13 +28,17 @@ public class HibernateUtils {
     
     private static final ThreadLocal<Session> threadSession = new ThreadLocal<Session>();
     private static final ThreadLocal<Transaction> threadTransaction = new ThreadLocal<Transaction>();
+    private static final ThreadLocal<Interceptor> threadInterceptor = new ThreadLocal<Interceptor>();
     
     static {
         try {            
+        	threadInterceptor.set(new ModificationInterceptor());
+        	
         	sessionFactory = new AnnotationConfiguration()
             //TODO : Hibernate - Trouver un moyen pour mettre le nom de fichier dans un fichier poroperties ou autre.
         	.configure("fr/umlv/ir3/emagine/ressource/hibernate.cfg.xml")
-            .buildSessionFactory();
+            .setInterceptor(threadInterceptor.get())
+        	.buildSessionFactory();
         } catch (Throwable ex) {
             log.error("Initial SessionFactory creation failed.", ex);
             throw new ExceptionInInitializerError(ex);
@@ -110,42 +116,14 @@ public class HibernateUtils {
         }
     }
     
-    public static Map<String, FieldModification> getPropertySnapShot(BaseEntity entity)
+    public static Interceptor getInterceptor()
     {
-    	//HashMap<String, FieldModification>  propertyOldValue = new HashMap<String, FieldModification>();
-		//ClassMetadata classMetadata = sessionFactory.getClassMetadata(User.class);
-		Map classesMetadata = sessionFactory.getAllClassMetadata();
-		
-		for(Object obj : classesMetadata.keySet())
-		{
-			//JoinedSubclassEntityPersister persiter = (JoinedSubclassEntityPersister)classesMetadata.get(obj);
-			ClassMetadata persiter = (ClassMetadata)classesMetadata.get(obj);
-			try
-			{
-				System.err.println("Class : " + obj);
-				String[] strings = persiter.getPropertyNames();
-				for(String string : strings)
-				{
-					Object object = persiter.getPropertyValue(entity, string, EntityMode.POJO);
-					System.err.println("Valeur : " + classesMetadata.get( object ));
-				}
-			}
-			catch(HibernateException e)
-			{
-				System.err.println("Exception");
-				e.printStackTrace();
-			}
-		}
-		//Map map = null;	
-		//classMetadata.getPropertyValuesToInsert(entity,map, (SessionImplementor) threadSession.get());
-	/*	System.out.println(classMetadata);
-		Object[] object = classMetadata.getPropertyValues(entity, null);
-		for(Object obj : object)
-		{
-			System.out.println(obj);
-		}*/
-
-		
-		return null;
+    	return threadInterceptor.get();
+    }
+    
+    public static Object getPropertyValue(String  name, BaseEntity entity)
+    {
+    	ClassMetadata classMetadata = sessionFactory.getClassMetadata(entity.getClass());
+    	return classMetadata.getPropertyValue(entity, name, EntityMode.POJO);
     }
 }
