0340f251274a737118abb1350258a491fd88aac0
lbarbisan <lbarbisan@06c2f47b-b305-0410-bc58-bf68df72c600>
2005-12-09 15:25:00 +0000

    Mise à jour du répertoire JUnit

---
+++ eMagine/TestSource/fr/umlv/ir3/emagine/apprentice/ApprenticeDAOTest.java
@@ -0,0 +1,61 @@
+package fr.umlv.ir3.emagine.apprentice;
+
+import junit.framework.TestCase;
+
+public class ApprenticeDAOTest extends TestCase {
+
+	public ApprenticeDAOTest(String arg0) {
+		super(arg0);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	/*
+	 * Test method for 'fr.umlv.ir3.emagine.apprentice.ApprenticeDAO.find(ApprenticeSearchParam)'
+	 */
+	public void testFind() {
+
+	}
+
+	/*
+	 * Test method for 'fr.umlv.ir3.emagine.util.base.BaseDAO.create(EntityType)'
+	 */
+	public void testCreate() {
+
+	}
+
+	/*
+	 * Test method for 'fr.umlv.ir3.emagine.util.base.BaseDAO.update(EntityType)'
+	 */
+	public void testUpdate() {
+
+	}
+
+	/*
+	 * Test method for 'fr.umlv.ir3.emagine.util.base.BaseDAO.retrieve(Class<? extends EntityType>, long)'
+	 */
+	public void testRetrieve() {
+
+	}
+
+	/*
+	 * Test method for 'fr.umlv.ir3.emagine.util.base.BaseDAO.delete(EntityType)'
+	 */
+	public void testDeleteEntityType() {
+
+	}
+
+	/*
+	 * Test method for 'fr.umlv.ir3.emagine.util.base.BaseDAO.delete(Collection<EntityType>)'
+	 */
+	public void testDeleteCollectionOfEntityType() {
+
+	}
+
+}
