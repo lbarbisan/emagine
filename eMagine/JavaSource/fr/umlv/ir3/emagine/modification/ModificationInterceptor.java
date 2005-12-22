a294feae847975367dab9e653874a699e00a2669
lbarbisan <lbarbisan@06c2f47b-b305-0410-bc58-bf68df72c600>
2005-12-22 17:43:49 +0000

    Mise à jour des modification

--- eMagine/JavaSource/fr/umlv/ir3/emagine/modification/ModificationInterceptor.java
+++ eMagine/JavaSource/fr/umlv/ir3/emagine/modification/ModificationInterceptor.java
@@ -1,29 +1,18 @@
 package fr.umlv.ir3.emagine.modification;
 
 import java.io.Serializable;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.util.Date;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Set;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hibernate.CallbackException;
 import org.hibernate.EmptyInterceptor;
-import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.type.Type;
 
-import fr.umlv.ir3.emagine.util.DAOManager;
-import fr.umlv.ir3.emagine.util.EMagineException;
-import fr.umlv.ir3.emagine.util.ManagerManager;
-import fr.umlv.ir3.emagine.util.base.BaseEntity;
 import fr.umlv.ir3.emagine.util.base.EditableEntity;
 
 /**
@@ -38,13 +27,8 @@ public class ModificationInterceptor extends EmptyInterceptor {
 	private Log log = LogFactory.getLog(ModificationInterceptor.class);
 
     private SessionFactory sessionFactory;
-    private static final String UPDATE = "update";
-    private static final String INSERT = "insert";
-    private static final String DELETE = "delete";
-    
-    private Set inserts = new HashSet();
-    private Set updates = new HashSet();
-    private Set deletes = new HashSet();
+   
+    private Map<Serializable, Modification> modifications = new HashMap<Serializable, Modification>();
 
 
     /**
@@ -61,21 +45,24 @@ public class ModificationInterceptor extends EmptyInterceptor {
      *      java.io.Serializable, java.lang.Object[], java.lang.Object[],
      *      java.lang.String[], net.sf.hibernate.type.Type[])
      */
-    public boolean onFlushDirty(Object obj, Serializable id, Object[] newValues, Object[] oldValues,
+    public boolean onFlushDirty(Object entity, Serializable id, Object[] newValues, Object[] oldValues,
             String[] properties, Type[] types) throws CallbackException
             {
 
     	//Vérifie que l'objet est bien un objet persitant modifiable
-        if (obj instanceof EditableEntity  && /* FIXME : Vérifié que l'utilisateur à les droit sur l'objet */true)
+        if (entity instanceof EditableEntity  && /* FIXME : Vérifié que l'utilisateur à les droit sur l'objet */true)
         {
-        	Modification modification = new Modification();
-        	try {
-        		DAOManager.getInstance().getBaseDAO().create(modification);
-			} catch (EMagineException e) {
-				// TODO EMagineException.e Not Implemented
-				e.printStackTrace();
-			}
-			
+        	Modification modification;
+        	
+        	if(modifications.containsKey(id)==true)
+        	{
+        		modification = new Modification();
+        	}
+        	else
+        	{
+        		modification = modifications.get(id);
+        	}
+
         	for(int index=0;index<properties.length;index++)
         	{
         		if(oldValues[index].equals(newValues[index])==false)
@@ -83,29 +70,34 @@ public class ModificationInterceptor extends EmptyInterceptor {
             			FieldModification<Object> fieldModification = new FieldModification<Object>();
             			fieldModification.setPropertyName(properties[index]);
             			fieldModification.setPropertyValue((Serializable)newValues[index]);
-            			try
-            			{
-            				DAOManager.getInstance().getBaseDAO().ge create(fieldModification);
-        				} catch (EMagineException e) {
-        					// TODO EMagineException.e Not Implemented
-        					e.printStackTrace();
+            			
+            			modification.getFieldsModifications().put(fieldModification.getPropertyName(), fieldModification);
+            			modification.setEditableEntity((EditableEntity)entity);
         				}
-        				modification.getFieldsModifications().put(fieldModification.getPropertyName(), fieldModification);
-        				try {
-							DAOManager.getInstance().getBaseDAO().update(modification);
-						} catch (EMagineException e) {
-							// TODO EMagineException.e Not Implemented
-							e.printStackTrace();
-						}
-        				((EditableEntity)obj).getModifications().add(modification);
-        				}
-	
         	}
+        	
+        	modifications.put(id, modification);
         	
         	newValues = oldValues;
         	return true;
         }
         return false;
     }
+
+    /**
+     * @see org.hibernate.Interceptor#postFlush(java.util.Iterator)
+     */
+	@Override
+	public void postFlush(Iterator entities) {
+		Session session = sessionFactory.openSession();
+		
+		for(Modification modification : modifications.values())
+		{
+			session.save(modification);
+			session.flush();
+		}
+		
+		modifications.clear();
+	}
  
 }
