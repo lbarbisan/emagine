6d5f541449d186aa448998d9427fedb39f80623a
lbarbisan <lbarbisan@06c2f47b-b305-0410-bc58-bf68df72c600>
2006-01-05 22:59:34 +0000

    Javadoc OK. Bug: Ticket #13

--- eMagine/JavaSource/fr/umlv/ir3/emagine/apprentice/absence/Absence.java
+++ eMagine/JavaSource/fr/umlv/ir3/emagine/apprentice/absence/Absence.java
@@ -12,12 +12,12 @@ import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.TemporalType;
-import javax.persistence.Transient;
 
 import fr.umlv.ir3.emagine.apprentice.Apprentice;
 import fr.umlv.ir3.emagine.modification.EditableEntity;
 
 /**
+ * This class represent an abscence
  * @author Administrateur
  *
  */
@@ -36,12 +36,101 @@ public class Absence extends EditableEntity {
 	private Date startDate;
 	@Basic(temporalType = TemporalType.TIMESTAMP)
 	private Date endDate;
-	@Transient
-	private int numberOfWorkedDays;
 	private boolean justification;
 	private String justificationComment;
 	
+	/**
+	 * Constructor reserved for hibernate
+	 */
 	protected Absence(){super();}
+	
+	/**
+	 * @return Returns the apprentice.
+	 */
+	public Apprentice getApprentice() {
+		return apprentice;
+	}
+
+	/**
+	 * @param apprentice The apprentice to set.
+	 */
+	public void setApprentice(Apprentice apprentice) {
+		this.apprentice = apprentice;
+	}
+
+	/**
+	 * @return Returns the endDate.
+	 */
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	/**
+	 * @param endDate The endDate to set.
+	 */
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	/**
+	 * @return Returns the justification.
+	 */
+	public boolean isJustification() {
+		return justification;
+	}
+
+	/**
+	 * @param justification The justification to set.
+	 */
+	public void setJustification(boolean justification) {
+		this.justification = justification;
+	}
+
+	/**
+	 * @return Returns the justificationComment.
+	 */
+	public String getJustificationComment() {
+		return justificationComment;
+	}
+
+	/**
+	 * @param justificationComment The justificationComment to set.
+	 */
+	public void setJustificationComment(String justificationComment) {
+		this.justificationComment = justificationComment;
+	}
+
+	/**
+	 * @return Returns the startDate.
+	 */
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	/**
+	 * @param startDate The startDate to set.
+	 */
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	/**
+	 * @return Returns the numberOfWorkedDays.
+	 */
+	//FIXME : Ticket #13 Mettre en place l'algorithme de calcule du nombre de jour travailler
+	public int getNumberOfWorkedDays() {
+		return 0;
+	}
+
+	/**
+	 * Create an absence with the default specfied parameter
+	 * @param justification true if this absence is justified, false otherwise
+	 * @param justificationComment comment for this justification, it can be null
+	 * @param startDate abscence's start date
+	 * @param endDate abscence's end date
+	 * @throws IllegalArgumentException, if the startDate is gretter than endDate
+	 */
+	
 	public Absence(boolean justification ,
 					String justificationComment,
 					Date startDate,
@@ -50,41 +139,14 @@ public class Absence extends EditableEntity {
 		super();
 		this.justification = justification;
 		this.justificationComment = justificationComment;
-		this.startDate = startDate;
-		this.endDate = endDate;
-	}
-	
-	public Date getEndDate() {
-		return endDate;
-	}
-	public void setEndDate(Date endDate) {
-		this.endDate = endDate;
-	}
-	public boolean isJustification() {
-		return justification;
-	}
-	public void setJustification(boolean justification) {
-		this.justification = justification;
-	}
-	public String getJustificationComment() {
-		return justificationComment;
-	}
-	public void setJustificationComment(String justificationComment) {
-		this.justificationComment = justificationComment;
-	}
-	public int getNumberOfWorkedDays() {
-		return numberOfWorkedDays;	// TODO : calculer en live le nombre de jour travaillés
-	}
-	public Date getStartDate() {
-		return startDate;
-	}
-	public void setStartDate(Date startDate) {
-		this.startDate = startDate;
-	}
-	public Apprentice getApprentice() {
-		return apprentice;
-	}
-	public void setApprentice(Apprentice apprentice) {
-		this.apprentice = apprentice;
+		if(startDate.after(endDate))
+		{
+			throw new IllegalArgumentException("startDate must be lower than endDate"); 
+		}
+		else
+		{
+			this.startDate = startDate;
+			this.endDate = endDate;
+		}
 	}
 }
