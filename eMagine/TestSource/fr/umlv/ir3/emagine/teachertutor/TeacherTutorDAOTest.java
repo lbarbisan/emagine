c885eb1586237c740f4302b6fa00dc6d65bfeec5
mmancel <mmancel@06c2f47b-b305-0410-bc58-bf68df72c600>
2006-01-06 00:07:24 +0000

    JUnit du DAO de teacherTutor

---
+++ eMagine/TestSource/fr/umlv/ir3/emagine/teachertutor/TeacherTutorDAOTest.java
@@ -0,0 +1,197 @@
+package fr.umlv.ir3.emagine.teachertutor;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import fr.umlv.ir3.emagine.apprentice.CountryEnum;
+import fr.umlv.ir3.emagine.apprentice.DepartmentEnum;
+import fr.umlv.ir3.emagine.apprentice.NationalityEnum;
+import fr.umlv.ir3.emagine.apprentice.SexEnum;
+import fr.umlv.ir3.emagine.event.Event;
+import fr.umlv.ir3.emagine.util.Address;
+import fr.umlv.ir3.emagine.util.base.BaseDAO;
+import fr.umlv.ir3.emagine.util.base.BaseDAOTest;
+import fr.umlv.ir3.emagine.util.search.SearchParams;
+import fr.umlv.ir3.emagine.util.search.SearchParamsImpl;
+
+public class TeacherTutorDAOTest extends BaseDAOTest<TeacherTutor> {
+
+	TeacherTutorDAO dao;
+	
+	public TeacherTutorDAOTest(String arg0) {
+		super(arg0);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		dao = new TeacherTutorDAO();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		dao = null;
+	}
+
+	@Override
+	protected BaseDAO<TeacherTutor> getDAO() {
+		return dao;
+	}
+
+	@Override
+	protected TeacherTutor createEntity() {
+		
+		Address address = new Address();
+		address.setCity("city");
+		address.setCountry(CountryEnum.FR);
+		address.setDepartment(DepartmentEnum.D01);
+		address.setPostalCode("postal");
+		address.setStreet("street");
+		
+		TeacherTutor teacherTutor = new TeacherTutor();
+		teacherTutor.setAddressPersonnal(address);
+		teacherTutor.setAddressProfessional(address);
+		
+		// TODO setApprentice();
+		//teacherTutor.setApprentice();
+		teacherTutor.setBirthdayCity("city");
+		teacherTutor.setBirthdayCountry(CountryEnum.FR);
+		teacherTutor.setEmail("mail");
+		teacherTutor.setEvents(new ArrayList<Event>());
+		teacherTutor.setFax("fax");
+		teacherTutor.setFirstName("firstname");
+		teacherTutor.setLastName("lastname");
+		teacherTutor.setMobilePhone("mobile");
+		teacherTutor.setNationality(NationalityEnum.FRENCH);
+		teacherTutor.setPhone("phone");
+		teacherTutor.setSex(SexEnum.MALE);
+		return teacherTutor;
+	}
+
+	@Override
+	protected Collection<TeacherTutor> createEntityCollection() {
+		
+		ArrayList<TeacherTutor> list = new ArrayList<TeacherTutor>();
+		
+		Address address1 = new Address();
+		address1.setCity("city");
+		address1.setCountry(CountryEnum.FR);
+		address1.setDepartment(DepartmentEnum.D01);
+		address1.setPostalCode("postal");
+		address1.setStreet("street");
+		Address address2 = new Address();
+		address2.setCity("city");
+		address2.setCountry(CountryEnum.FR);
+		address2.setDepartment(DepartmentEnum.D01);
+		address2.setPostalCode("postal");
+		address2.setStreet("street");
+		
+		TeacherTutor teacherTutor1 = new TeacherTutor();
+		teacherTutor1.setAddressPersonnal(address1);
+		teacherTutor1.setAddressProfessional(address1);
+		// TODO setApprentice();
+		//teacherTutor.setApprentice();
+		teacherTutor1.setBirthdayCity("city");
+		teacherTutor1.setBirthdayCountry(CountryEnum.FR);
+		teacherTutor1.setEmail("mail");
+		teacherTutor1.setEvents(new ArrayList<Event>());
+		teacherTutor1.setFax("fax");
+		teacherTutor1.setFirstName("firstname");
+		teacherTutor1.setLastName("lastname");
+		teacherTutor1.setMobilePhone("mobile");
+		teacherTutor1.setNationality(NationalityEnum.FRENCH);
+		teacherTutor1.setPhone("phone");
+		teacherTutor1.setSex(SexEnum.MALE);
+		
+		TeacherTutor teacherTutor2 = new TeacherTutor();
+		teacherTutor2.setAddressPersonnal(address2);
+		teacherTutor2.setAddressProfessional(address2);
+		// TODO setApprentice();
+		//teacherTutor.setApprentice();
+		teacherTutor2.setBirthdayCity("city");
+		teacherTutor2.setBirthdayCountry(CountryEnum.FR);
+		teacherTutor2.setEmail("mail");
+		teacherTutor2.setEvents(new ArrayList<Event>());
+		teacherTutor2.setFax("fax");
+		teacherTutor2.setFirstName("firstname");
+		teacherTutor2.setLastName("lastname");
+		teacherTutor2.setMobilePhone("mobile");
+		teacherTutor2.setNationality(NationalityEnum.FRENCH);
+		teacherTutor2.setPhone("phone");
+		teacherTutor2.setSex(SexEnum.MALE);
+		
+		list.add(teacherTutor1);
+		list.add(teacherTutor2);
+		
+		return list;
+	}
+
+	@Override
+	protected void updateEntity(TeacherTutor entity) {
+		Address address = new Address();
+		address.setCity("new city");
+		address.setCountry(CountryEnum.EN);
+		address.setDepartment(DepartmentEnum.D02);
+		address.setPostalCode("new postal");
+		address.setStreet("new street");
+		entity.setAddressPersonnal(address);
+		entity.setAddressProfessional(address);
+		
+		// TODO setApprentice();
+		//teacherTutor.setApprentice();
+		entity.setBirthdayCity("new city");
+		entity.setBirthdayCountry(CountryEnum.FR);
+		entity.setEmail("new mail");
+		entity.setEvents(new ArrayList<Event>());
+		entity.setFax("new fax");
+		entity.setFirstName("new firstname");
+		entity.setLastName("new lastname");
+		entity.setMobilePhone("new mobile");
+		entity.setNationality(NationalityEnum.FRENCH);
+		entity.setPhone("new phone");
+		entity.setSex(SexEnum.MALE);
+	}
+
+	@Override
+	protected SearchParams createSearchParams() {
+		SearchParamsImpl searchParams = new SearchParamsImpl();
+		searchParams.setField("firstName" , "name");
+		searchParams.setField("lastName" , "name");
+		return searchParams;
+	}
+
+	@Override
+	protected void compareEntity(TeacherTutor entity1, TeacherTutor entity2) {
+		assertTrue(conditionCheck(entity1.getAddressPersonnal(), entity2.getAddressPersonnal()));
+		assertTrue(conditionCheck(entity1.getAddressProfessional(), entity2.getAddressProfessional()));
+		assertTrue(conditionCheck(entity1.getApprentice(), entity2.getApprentice()));
+		assertTrue(conditionCheck(entity1.getBirthdayCity(), entity2.getBirthdayCity()));
+		assertTrue(conditionCheck(entity1.getBirthdayCountry(), entity2.getBirthdayCountry()));
+		assertTrue(conditionCheck(entity1.getBirthdayDate(), entity2.getBirthdayDate()));
+		assertTrue(conditionCheck(entity1.getBirthdayDepartment(), entity2.getBirthdayDepartment()));
+		assertTrue(conditionCheck(entity1.getEmail(), entity2.getEmail()));
+		assertTrue(conditionCheck(entity1.getEvents(), entity2.getEvents()));
+		assertTrue(conditionCheck(entity1.getFax(), entity2.getFax()));
+		assertTrue(conditionCheck(entity1.getFirstName(), entity2.getFirstName()));
+		assertTrue(conditionCheck(entity1.getId(), entity2.getId()));
+		assertTrue(conditionCheck(entity1.getLastName(), entity2.getLastName()));
+		assertTrue(conditionCheck(entity1.getMobilePhone(), entity2.getMobilePhone()));
+		assertTrue(conditionCheck(entity1.getModifications(), entity2.getModifications()));
+		assertTrue(conditionCheck(entity1.getNationality(), entity2.getNationality()));
+		assertTrue(conditionCheck(entity1.getPhone(), entity2.getPhone()));
+		assertTrue(conditionCheck(entity1.getSex(), entity2.getSex()));
+	}
+	/**
+	 * Renvoi True si la condition est vérifier False Sinon
+	 * @param <ConditionnalEntityType>
+	 * @param Obj1 Première Object à Tester
+	 * @param Obj2 Deuxième Object à Tester
+	 * @return True si la condition est vérifier False Sinon
+	 */
+	private<ConditionnalEntityType> boolean conditionCheck(ConditionnalEntityType Obj1, ConditionnalEntityType Obj2)
+	{
+			return (!(Obj1!=null ^ Obj2!=null)) &&
+					((Obj1==null && Obj2==null) || 
+					Obj1.equals(Obj2));
+	}
+
+}
