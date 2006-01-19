4240b505aa7c296089f2775fe39b1e3cf5792998
netangel <netangel@06c2f47b-b305-0410-bc58-bf68df72c600>
2006-01-19 21:07:53 +0000

    Correction : mise en Entity de NationalityEnum

--- eMagine/JavaSource/fr/umlv/ir3/emagine/apprentice/NationalityEnum.java
+++ eMagine/JavaSource/fr/umlv/ir3/emagine/apprentice/NationalityEnum.java
@@ -4,12 +4,16 @@
 package fr.umlv.ir3.emagine.apprentice;
 
 
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+
 import fr.umlv.ir3.emagine.util.EmagineEnum;
 
 /**
  * @author Laurent
  *
  */
+@Entity(access=AccessType.FIELD)
 public class NationalityEnum extends EmagineEnum{
 	public NationalityEnum(String string) {
 		super(string);
