821a830ac79b41564121a539b2dd9be268e6459c
lbarbisan <lbarbisan@06c2f47b-b305-0410-bc58-bf68df72c600>
2006-01-19 05:28:06 +0000

    Test Right DAO

--- eMagine/JavaSource/fr/umlv/ir3/emagine/user/profile/RightManagerImpl.java
+++ eMagine/JavaSource/fr/umlv/ir3/emagine/user/profile/RightManagerImpl.java
@@ -1,10 +1,20 @@
 package fr.umlv.ir3.emagine.user.profile;
 
+import java.util.List;
+
 import fr.umlv.ir3.emagine.util.DAOManager;
 import fr.umlv.ir3.emagine.util.base.BaseManagerImpl;
 
 public class RightManagerImpl extends BaseManagerImpl<Right, RightDAO> implements RightManager {
 	public RightDAO getDAO() {
 		return DAOManager.getInstance().getRightDAO();
+	}
+	
+	public List<String> findCategories() {
+		return getDAO().findCategories();
+	}
+	
+	public List<Right> findRightsFor(String category){
+		return getDAO().findRightsFor(category);
 	}
 }
