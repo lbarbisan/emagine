b042c810087bc82e0838e7e2ab35a6aff85e85df
netangel <netangel@06c2f47b-b305-0410-bc58-bf68df72c600>
2006-02-05 14:53:28 +0000

    Amélioration et réfélchissage de crâne sur les extractions. (oublie d'un fichier) ref #23

---
+++ eMagine/JavaSource/fr/umlv/ir3/emagine/extraction/ExtractionForm.java
@@ -0,0 +1,43 @@
+/**
+ *  
+ */
+package fr.umlv.ir3.emagine.extraction;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionMapping;
+
+/**
+ * Form that represents the final submited extraction form. It is attached to the action that generate the extracted file.
+ * @author aogier
+ */
+public interface ExtractionForm<O> extends ExtractionShowForm, ExtractionParams {
+	/**
+	 * @return <code>true</code> if the user wants to save the configuration, <code>false</code> otherwise.
+	 */
+	public boolean isSaveConfig();
+
+	/**
+	 * Sets <code>true</code> if the user wants to save the configuration, <code>false</code> otherwise.
+	 * @param saveConfig
+	 */
+	public void setSaveConfig(boolean saveConfig);
+
+	/**
+	 * @return the name of the object that contains the list the user wants to extract. That name corresponds to the session attribute name of the object that contains the list.
+	 */
+	public String getExtractionObjectName();
+
+	/**
+	 * @param mapping
+	 * @param form
+	 * @param request
+	 * @param response
+	 * @return the extraction list corresponding to the extraction list name.
+	 */
+	public List<O> getExtractionList(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response);
+}
